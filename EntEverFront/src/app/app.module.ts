1cd14423a6b20a5b2e710460b6f989d91e0f7e84
Frankwang25 <gmfrankwang@gmail.com>
2021-06-09 05:47:11 +1000

    Commit for component init

--- EntEverFront/src/app/app.module.ts
+++ EntEverFront/src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { OverviewComponent } from './overview/overview.component';
+import { DiscussionComponent } from './discussion/discussion.component';
+import { HeaderComponent } from './frame/header/header.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    OverviewComponent,
+    DiscussionComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path:'', component: HomeComponent},
+      { path:'overview', component: OverviewComponent},
+      { path:'discussion', component: DiscussionComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
