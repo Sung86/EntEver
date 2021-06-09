728367bafd4b81ae922390ff215f7c36888c0402
jinzhih <jessie.houjinzhi@gmail.com>
2021-06-09 20:51:09 +1000

    change css

--- EntEverFront/src/app/home/homematerial.module.ts
+++ EntEverFront/src/app/home/homematerial.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-    imports: [MatButtonModule],
-    exports: [MatButtonModule]
+    imports: [MatButtonModule,MatDividerModule],
+    exports: [MatButtonModule,MatDividerModule]
 })
 export class HomeMaterialModule{}
