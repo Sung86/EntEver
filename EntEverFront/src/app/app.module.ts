687b976f198737d53a6c00afb47e058f9357b447
jinzhih <jessie.houjinzhi@gmail.com>
2021-06-09 20:17:08 +1000

    home session1 2

--- EntEverFront/src/app/app.module.ts
+++ EntEverFront/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -7,6 +8,7 @@ import { OverviewComponent } from './overview/overview.component';
 import { DiscussionComponent } from './discussion/discussion.component';
 import { HeaderComponent } from './frame/header/header.component';
 import { RouterModule } from '@angular/router';
+import { HomeMaterialModule } from './home/homematerial.module';
 
 @NgModule({
   declarations: [
@@ -18,6 +20,8 @@ import { RouterModule } from '@angular/router';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    HomeMaterialModule,
     RouterModule.forRoot([
       { path:'', component: HomeComponent},
       { path:'overview', component: OverviewComponent},
